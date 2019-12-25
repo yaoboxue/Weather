c1b6e97e05922af37a0004126c56fd337e9d2e02
yaoboxue <yaoboshi126@qq.com>
2019-12-26 01:53:16 +0800

    third

--- app/src/main/java/com/yaoboxue/weather/CityWeatherFragment.java
+++ app/src/main/java/com/yaoboxue/weather/CityWeatherFragment.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,20 +15,29 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.google.gson.Gson;
 import com.yaoboxue.weather.Base.BaseFragment;
 import com.yaoboxue.weather.Bean.WeatherBean;
 import com.yaoboxue.weather.DB.DBmanager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass
  */
 
-public class CityWeatherFragment extends BaseFragment implements View.OnClickListener {
+public class CityWeatherFragment extends BaseFragment implements View.OnClickListener, OnChartValueSelectedListener, OnChartGestureListener {
     private static final String TAG = "MainActivity";
     private static final String Key = "&key=8c1c69f446886d0656af1c89161aed0c";
     private static int FIRST = 0;
@@ -38,6 +48,10 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
     ImageView dayIma;
     LinearLayout futureLayout;
     LinearLayout lineChart;
+
+    //折线图
+    private RecyclerView recyclerView;
+    List<Integer> Temdata;
 
     WeatherBean.ResultBean.TodayBean indexList = new WeatherBean.ResultBean.TodayBean();
 
@@ -54,6 +68,23 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_city_weather, container, false);
         //初始化控件
+
+        //
+        recyclerView = view.findViewById(R.id.recycler_view);
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        manager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(manager);
+
+        Temdata = new ArrayList<>();
+        Temdata.add(24);
+        Temdata.add(18);
+        Temdata.add(22);
+        Temdata.add(19);
+        Temdata.add(23);
+        Temdata.add(24);
+        Temdata.add(26);
+        Temdata.add(28);
+
         initView(view);
         //通过activity传值取到当前fragment加载的是那个地方的天气情况
         Bundle data = getArguments();
@@ -61,8 +92,13 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
         Log.v(TAG,"city"+city);
         String url = url1+city+url2;
         //调取父类获取数据的方法
+//        if (FIRST == 1){
+//            loadData(url);
+//            Log.v(TAG,"14"+FIRST);
+//        }
         loadData(url);
         Log.v(TAG,"1");
+
         return view;
 
     }
@@ -71,20 +107,45 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
 
     @Override
     public void onSuccess(String result) {
-        //解析并展示数据
-        if (FIRST == 0) {
-            parseShowData(result);
-
-        }else if (FIRST == 1){
-            dealData(result);
-        }
-        //更新数据
-       int i = DBmanager.updataInfoByCity(city,result);
+        parseShowData(result);
+        int i = DBmanager.updataInfoByCity(city,result);
         if (i<=0){
             //更新数据库失败,说明没有这个城市，增加这个城市记录；
             DBmanager.addCityInfo(city,result);
         }
         Log.v(TAG,"第一个成功"+result);
+        //解析并展示数据
+     /*   if (FIRST == 0) {
+            parseShowData(result);
+            int i = DBmanager.updataInfoByCity(city,result);
+            if (i<=0){
+                //更新数据库失败,说明没有这个城市，增加这个城市记录；
+                DBmanager.addCityInfo(city,result);
+            }
+            Log.v(TAG,"第一个成功"+result);
+            Log.v(TAG,"13"+FIRST);
+
+        }else if (FIRST == 1){
+            dealData(result);
+            int i = DBmanager.updataInfoByCity(city,result);
+            if (i<=0){
+                //更新数据库失败,说明没有这个城市，增加这个城市记录；
+                DBmanager.addCityInfo(city,result);
+            }
+            Log.v(TAG,"第一个成功"+result);
+            Log.v(TAG,"12"+FIRST);
+        }
+        //更新数据
+        parseShowData(result);
+        int i = DBmanager.updataInfoByCity(city,result);
+        if (i<=0){
+            //更新数据库失败,说明没有这个城市，增加这个城市记录；
+            DBmanager.addCityInfo(city,result);
+        }
+        Log.v(TAG,"第一个成功"+result);
+        Log.v(TAG,"1"+FIRST);*/
+
+
     }
 
     @Override
@@ -135,8 +196,11 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
             iconditiontes.setText(dataBean.getWeather());
             itempRangtex.setText(dataBean.getTemperature());
         }
+
+
     }
 
+    //未使用
     public void dealData(String result){
 
         WeatherBean weatherBean = new Gson().fromJson(result, WeatherBean.class);
@@ -157,6 +221,10 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
         temptex.setText(resultsBean.getSk().getTemp()+"℃");
         timeTex.setText(resultsBean.getSk().getTime());
 
+        //
+
+
+
         List<WeatherBean.ResultBean.FutureBean> futureList = resultsBean.getFuture();
         futureList.remove(0);
         Log.v(TAG,"大小"+futureList.size());
@@ -174,10 +242,29 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
             itempRangtex.setText(dataBean.getTemperature());
         }
 
-        View itemView = LayoutInflater.from(getActivity()).inflate(R.layout.linechar, null);
-        itemView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        lineChart.addView(itemView);
-        mLineChar
+
+
+        //未使用，废弃
+        View lineView = LayoutInflater.from(getActivity()).inflate(R.layout.linechar, null);
+        lineView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        lineChart.addView(lineView);
+        com.github.mikephil.charting.charts.LineChart mLineChar = lineView.findViewById(R.id.mLineChar);
+        //设置手势滑动事件
+        mLineChar.setOnChartGestureListener(this);
+        //设置数值选择监听
+        mLineChar.setOnChartValueSelectedListener(this);
+        //后台绘制
+        mLineChar.setDrawGridBackground(false);
+        //设置描述文本
+        mLineChar.getDescription().setEnabled(false);
+        //设置支持触控手势
+        mLineChar.setTouchEnabled(true);
+        //设置缩放
+        mLineChar.setDragEnabled(true);
+        //设置推动
+        mLineChar.setScaleEnabled(true);
+        //如果禁用,扩展可以在x轴和y轴分别完成
+        mLineChar.setPinchZoom(true);
 
     }
 
@@ -199,7 +286,13 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
         raysIndexTex = view.findViewById(R.id.index_rays);
         dayIma = view.findViewById(R.id.frg_weather_icon);
         futureLayout = view.findViewById(R.id.frg_middle_layout);
-        lineChart = view.findViewById(R.id.frg_lineChart);
+        recyclerView = view.findViewById(R.id.recycler_view);
+       // lineChart = view.findViewById(R.id.frg_lineChart);
+
+        //设置折线图
+        Hour_Adapter adapter = new Hour_Adapter(getActivity(),Temdata);
+        recyclerView.setAdapter(adapter);
+
 
         //下拉刷新
         swipeRefreshLayout = view.findViewById(R.id.swipe_refresh);
@@ -207,7 +300,8 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                requestWeather(city);
+              //  FIRST = 1;
+               // requestWeather(city);
                 swipeRefreshLayout.setRefreshing(false);
                new Handler().postDelayed(new Runnable() {
                     @Override
@@ -230,6 +324,7 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
         FIRST = 1;
         String url = url1 + city + url2;
         loadData(url);
+
 
 
     }
@@ -277,5 +372,57 @@ public class CityWeatherFragment extends BaseFragment implements View.OnClickLis
 
         }
         builder.create().show();
+    }
+
+    //事件监听重写
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
+
+    //手势方法重写
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
     }
 }
